71aca15e68f2f422b5a0026f9b96c24e109a3e43
Alex Robertson <arobertson335@gmail.com>
2020-05-25 23:12:38 -0700

    Saves progress on user functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ def index
   @users = User.all
 end
 
-end
+def show
+  user_id = params[:id]
+  @user = User.find_by(id: user_id)
+
+  if @user == nil
+    head :not_found
+    return
+  end
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
   
-  validates :username , presence: true , uniqueness: true 
+  #TODO: Work on validations 
+  # validates :username , presence: true , uniqueness: true 
 end
